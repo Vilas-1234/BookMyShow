f6c137d400998a310712822e9be796688a595478
vilas <annupokarne2020@gmail.com>
2023-09-02 11:26:46 +0530

    vilas did some changes

--- src/Examquestion/replaceaws.java
+++ src/Examquestion/replaceaws.java
@@ -10,14 +10,14 @@ public class replaceaws {
         }
         System.out.println (st);
         String sst = "     Vilas         manik    pokarne ";
-//    int count=0;
-//        StringTokenizer stt= new StringTokenizer (sst);
-//        while(stt.hasMoreElements ()==true){
-//            System.out.println (stt.nextElement () );
-//            count++;
-//        }
-//        System.out.println (count);
-//    }
+    int count=0;
+        StringTokenizer stt= new StringTokenizer (sst);
+        while(stt.hasMoreElements ()==true){
+            System.out.println (stt.nextElement () );
+            count++;
+        }
+        System.out.println (count);
+   }
 
     }
 }
