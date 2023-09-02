a71f19d78b1dc3608d58d472592b7a0b56a15942
vilas <annupokarne2020@gmail.com>
2023-09-02 10:54:32 +0530

    add a new file and i can makes a some changes

--- src/Vilaspatil.java
+++ src/Vilaspatil.java
@@ -2,6 +2,8 @@ public class Vilaspatil{
 
 public static void main(String args[]){
 
+//yes we can done by the all date to transfer into remote or centrlized repository...
+
   System.out.println("I am vilas patil");
 }
 }
